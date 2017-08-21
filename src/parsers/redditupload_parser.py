0fea7781ddd941e31bc3351897e74d4ff5f6f10f
Charlie Corner <charliecorner@users.noreply.github.com>
2017-08-20 20:51:56 -0400

    Fix the way redditupload links should be handled

--- src/parsers/redditupload_parser.py
+++ src/parsers/redditupload_parser.py
@@ -11,16 +11,9 @@ class Redditupload(BaseParser):
     def get_images(self, post):
         images = []
 
-        html_source = requests.get(post.url).text
-        soup = BeautifulSoup(html_source, "lxml")
-        match = soup.select("img")
-
-        if not match:
-            raise NotAbleToDownloadException("Wasn't able to download %s as the image couldn't be found" % post.url)
-
-        image_url = match[0]["src"]
-        image_url = tidy_up_url(image_url) + ".jpg"
-        image_file = image_url[image_url.rfind('/') + 1:]
+        image_url = post.url
+        image_file = tidy_up_url(image_url) + ".jpg"
+        image_file = image_file[image_file.rfind('/') + 1:]
 
         images.append(Image(image_url, post, image_file))
 
