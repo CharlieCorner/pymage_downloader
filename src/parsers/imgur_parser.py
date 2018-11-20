66aa915130600f7dfec90b7e5df0da764bd2c52d
Charles Corner <charliecorner@users.noreply.github.com>
2018-11-20 00:04:34 -0600

    Refactor the ImgurAPIParser to user a get_file_name_from_url method
    This also abstracts the ImgurAPI from knowing the internal structure of
    the Image object for PyMage so that it can focus on just querying the
    API.

--- src/parsers/imgur_parser.py
+++ src/parsers/imgur_parser.py
@@ -30,18 +30,22 @@ class ImgurBaseParser(BaseParser):
         if image_url.endswith("gifv"):
             image_url = image_url.replace("gifv", "mp4")
 
-        image_file = image_url[image_url.rfind('/') + 1:]
+        image_file = ImgurBaseParser.get_file_name_from_url(image_url)
 
         image = Image(image_url, post, image_file)
 
         return image
+
+    @staticmethod
+    def get_file_name_from_url(url: str) -> str:
+        return url[url.rfind('/') + 1:]
 
 
 class ImgurParser(ImgurBaseParser):
     def __init__(self) -> None:
         self._imgur_url_pattern = re.compile(r"(imgur.com/(.*))(\?.*)?")
 
-    def get_images(self, post):
+    def get_images(self, post) -> list:
         images = []
 
         if "/a/" in post.url or "/gallery/" in post.url:
@@ -59,9 +63,8 @@ class ImgurParser(ImgurBaseParser):
                 raise NotAbleToDownloadException("Couldn't process %s" % post.url)
 
             for m in matches:
-                image_url = m['src']
-                image_url = tidy_up_url(image_url)
-                image_file = image_url[image_url.rfind('/') + 1:]
+                image_url = tidy_up_url(m['src'])
+                image_file = ImgurBaseParser.get_file_name_from_url(image_url)
                 images.append(Image(image_url, post, image_file))
 
         elif ImgurParser.is_imgur_direct_url(post.get("get")):
@@ -90,10 +93,26 @@ class ImgurParser(ImgurBaseParser):
                 image_url = match[0]['src']
 
             image_url = tidy_up_url(image_url)
-            image_file = image_url[image_url.rfind('/') + 1:]
+            image_file = ImgurParser.get_file_name_from_url(image_url)
 
             images.append(Image(image_url, post, image_file))
         else:
             raise NotAbleToDownloadException("Couldn't process %s" % post.url)
 
         return images
+
+
+class ImgurAPIParser(ImgurBaseParser):
+
+    def get_images(self, post: dict) -> list:
+        # First check if it is a direct URL so that we avoid querying the API
+        if ImgurAPIParser.is_imgur_direct_url(post.get("url")):
+            return [ImgurAPIParser.get_image_from_direct_url(post)]
+
+        urls = ImgurAPI.get_image_urls(post)
+        image_entities = []
+
+        for u in urls:
+            image_entities.append(Image(u, post, ImgurAPIParser.get_file_name_from_url(u)))
+
+        return image_entities
