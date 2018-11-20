842c6c02c322d854eb628d3e39f5d1d3f3cce061
Charles Corner <charliecorner@users.noreply.github.com>
2018-11-19 23:38:59 -0600

    Add an ImgurBaseParse that will provide utils for both the ImgurParser and ImgurAPIParser

--- src/parsers/imgur_parser.py
+++ src/parsers/imgur_parser.py
@@ -1,9 +1,10 @@
-import logging
 import re
+from abc import abstractmethod
 
 import requests
 from bs4 import BeautifulSoup
 
+from api.imgur.imgur_api import ImgurAPI
 from exceptions.pymage_exceptions import NotAbleToDownloadException
 from models.image import Image
 from parsers.base_parser import BaseParser
@@ -12,7 +13,31 @@ from utils.utils import *
 LOGGER = logging.getLogger(__name__)
 
 
-class ImgurParser(BaseParser):
+class ImgurBaseParser(BaseParser):
+
+    @abstractmethod
+    def get_images(self, post):
+        raise NotImplementedError("A concrete parser must be used")
+
+    @staticmethod
+    def is_imgur_direct_url(url: str) -> bool:
+        return tidy_up_url(url)[url.rfind(".") + 1:] in ["jpeg", "png", "jpg", "gif", "gifv"]
+
+    @staticmethod
+    def get_image_from_direct_url(post: dict) -> Image:
+        image_url = tidy_up_url(post.get("url"))
+
+        if image_url.endswith("gifv"):
+            image_url = image_url.replace("gifv", "mp4")
+
+        image_file = image_url[image_url.rfind('/') + 1:]
+
+        image = Image(image_url, post, image_file)
+
+        return image
+
+
+class ImgurParser(ImgurBaseParser):
     def __init__(self) -> None:
         self._imgur_url_pattern = re.compile(r"(imgur.com/(.*))(\?.*)?")
 
@@ -39,16 +64,9 @@ class ImgurParser(BaseParser):
                 image_file = image_url[image_url.rfind('/') + 1:]
                 images.append(Image(image_url, post, image_file))
 
-        elif tidy_up_url(post.url)[post.url.rfind(".") + 1:] in ["jpeg", "png", "jpg", "gif", "gifv"]:
+        elif ImgurParser.is_imgur_direct_url(post.get("get")):
             # This is a direct url
-            image_url = tidy_up_url(post.url)
-
-            if image_url.endswith("gifv"):
-                image_url = image_url.replace("gifv", "mp4")
-
-            image_file = image_url[image_url.rfind('/') + 1:]
-
-            images.append(Image(image_url, post, image_file))
+            images.append(ImgurParser.get_image_from_direct_url(post))
 
         elif "imgur.com/" in post.url:
             # This is an Imgur page with a single image
@@ -70,7 +88,6 @@ class ImgurParser(BaseParser):
                     raise NotAbleToDownloadException("Couldn't process %s" % post.url)
 
                 image_url = match[0]['src']
-
 
             image_url = tidy_up_url(image_url)
             image_file = image_url[image_url.rfind('/') + 1:]
